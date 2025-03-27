49cb98db1a54f164276501970e27aa555bc60aae
Anna-fog <anna49.fog@yandex.ru>
2025-03-27 10:52:30 +0100

    Updates projects

--- src/components/projects/projects.ts
+++ src/components/projects/projects.ts
@@ -56,16 +56,77 @@ export const Projects: ProjectItemType[] = [
 
 export const PetProjects: ProjectItemType[] = [
   {
-    title: 'Woodoo',
-    description: "An infrared sauna's main page.",
-    image: 'woodoo',
-    link: 'http://woodoo.3dw.by/',
+    title: 'Sweet Inner Radiance',
+    description: 'Jewelry store',
+    image: 'sweet-inner-radiance',
+    link: 'https://sweet-inner-radiance.3dw.by',
+  },
+  {
+    title: 'Leela',
+    description: 'Transformation game Leela.',
+    image: 'leela',
+    link: 'https://leela.3dw.by/',
+  },
+  {
+    title: 'Portfolio 1.0',
+    description: 'Landing page of a previous portfolio.',
+    image: 'portfolio',
+    link: 'https://portfolio.3dw.by/',
+  },
+  {
+    title: 'Quotes',
+    description: 'A web page featuring a list of quotes.',
+    image: 'quotes',
+    link: 'http://quotes.3dw.by/',
+  },
+  {
+    title: 'MusX',
+    description:
+      'A music player that lets you add and listen to your songs from a personal account.',
+    image: 'musx',
+    link: 'http://musx.3dw.by/',
+  },
+  {
+    title: 'Memory game',
+    description: 'Memory card game.',
+    image: 'memory-game',
+    link: 'http://memory-game.3dw.by/',
+  },
+  {
+    title: 'Notes App',
+    description: 'A simple JavaScript app for note-taking.',
+    image: 'notes-app',
+    link: 'http://notes-app.3dw.by/',
   },
   {
     title: 'Weather App',
     description: 'A simple page providing weather and air quality information.',
     image: 'weather-app',
     link: 'http://weather-app.3dw.by/',
+  },
+  {
+    title: 'Paint App',
+    description: 'A simple painting tool.',
+    image: 'paint',
+    link: 'https://paint-app.3dw.by/',
+  },
+  {
+    title: 'Flight game',
+    description: 'A flight game built with JavaScript and Canvas.',
+    image: 'flight',
+    link: 'https://flight-game.3dw.by/',
+  },
+  {
+    title: 'Woodoo',
+    description: "An infrared sauna's main page.",
+    image: 'woodoo',
+    link: 'http://woodoo.3dw.by/',
+  },
+  {
+    title: 'Guess Number',
+    description: 'A simple JavaScript game to test your intuition.',
+    image: 'guess-number',
+    link: 'http://guess-number.3dw.by/',
   },
   {
     title: 'Molisana',
@@ -84,60 +145,5 @@ export const PetProjects: ProjectItemType[] = [
     description: 'Landing page for floor screed services.',
     image: 'perfect-floor',
     link: 'http://perfectfloor.3dw.by/',
-  },
-  {
-    title: 'Guess Number',
-    description: 'A simple JavaScript game to test your intuition.',
-    image: 'guess-number',
-    link: 'http://guess-number.3dw.by/',
-  },
-  {
-    title: 'Notes App',
-    description: 'A simple JavaScript app for note-taking.',
-    image: 'notes-app',
-    link: 'http://notes-app.3dw.by/',
-  },
-  {
-    title: 'Memory game',
-    description: 'Memory card game.',
-    image: 'memory-game',
-    link: 'http://memory-game.3dw.by/',
-  },
-  {
-    title: 'MusX',
-    description:
-      'A music player that lets you add and listen to your songs from a personal account.',
-    image: 'musx',
-    link: 'http://musx.3dw.by/',
-  },
-  {
-    title: 'Quotes',
-    description: 'A web page featuring a list of quotes.',
-    image: 'quotes',
-    link: 'http://quotes.3dw.by/',
-  },
-  {
-    title: 'Paint App',
-    description: 'A simple painting tool.',
-    image: 'paint',
-    link: 'https://paint-app.3dw.by/',
-  },
-  {
-    title: 'Flight game',
-    description: 'A flight game built with JavaScript and Canvas.',
-    image: 'flight',
-    link: 'https://flight-game.3dw.by/',
-  },
-  {
-    title: 'Portfolio 1.0',
-    description: 'Landing page of a previous portfolio.',
-    image: 'portfolio',
-    link: 'https://portfolio.3dw.by/',
-  },
-  {
-    title: 'Leela',
-    description: 'Transformation game Leela.',
-    image: 'leela',
-    link: 'https://leela.3dw.by/',
   },
 ]
